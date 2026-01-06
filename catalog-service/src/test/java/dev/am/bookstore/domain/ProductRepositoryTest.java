fb3648dc506bd8cbbce032ba0baf07b6ae8e9fb7
Andres Merida <andresmerida@Andress-MacBook-Pro.local>
2026-01-05 23:45:29 -0400

    adding test repository

---
+++ catalog-service/src/test/java/dev/am/bookstore/domain/ProductRepositoryTest.java
@@ -0,0 +1,15 @@
+package dev.am.bookstore.domain;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class ProductRepositoryTest {
+
+    @BeforeEach
+    void setUp() {}
+
+    @Test
+    void findByCode() {}
+}
