d4a581bf935300a8f874839f91c155b2be39a364
Andres Merida <andresmerida@Andress-MacBook-Pro.local>
2026-01-20 23:40:03 -0400

    Improving functionalities applying Liskop Substitution Principle to some functionionalities that violate this principle

---
+++ catalog-service/src/main/java/dev/am/bookstore/web/practice/solid/lsp/Rectangle.java
@@ -0,0 +1,16 @@
+package dev.am.bookstore.web.practice.solid.lsp;
+
+public class Rectangle implements Shape {
+    private final double width;
+    private final double height;
+
+    public Rectangle(double width, double height) {
+        this.width = width;
+        this.height = height;
+    }
+
+    @Override
+    public double calculateArea() {
+        return width * height;
+    }
+}
---
+++ catalog-service/src/main/java/dev/am/bookstore/web/practice/solid/lsp/Square.java
@@ -0,0 +1,14 @@
+package dev.am.bookstore.web.practice.solid.lsp;
+
+public class Square implements Shape {
+    private final double side;
+
+    public Square(double side) {
+        this.side = side;
+    }
+
+    @Override
+    public double calculateArea() {
+        return side * side;
+    }
+}
